0faedcd66d35879cc6d494274347aeecffbbc3e3
Abrahan <ajvilla1999@gmail.com>
2023-07-03 17:52:44 -0500

    arrglando el slider

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-const prev = document.querySelector('.prev')
-const next = document.querySelector('.next')
-const slider = document.querySelector('.slider')
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const slider = document.querySelector('.slider');
+const prueba = document.querySelector('.box__container-proyect');
+const imagen = document.querySelectorAll('.img_proyect');
+const fondo = document.getElementById('carta');
+console.log(imagen);
+console.log(fondo);
+
+imagen.forEach(img => {
+    img.addEventListener('click', (a) => {
+        let src = a.target.getAttribute('src')
+        fondo.style.backgroundImage = `url(${src})`;
+        console.log(src);
+    })
+});
 
 prev.addEventListener('click', () => {
-    slider.scrollLeft -= 700
-    console.log(slider)
+    slider.scrollLeft -= 230;
 })
 
 next.addEventListener('click', () => {
-    slider.scrollLeft += 300
-    console.log(slider)
+    slider.scrollLeft += 228;
 })
-
-// slider.addEventListener('click', () => )
