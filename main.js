eff37b3bfa81cd50e5b2ece047de798bad5686a9
Abrahan <ajvilla1999@gmail.com>
2023-07-07 10:39:29 -0500

    Finalizando maquetado 02

--- main.js
+++ main.js
@@ -8,25 +8,14 @@ imagenes.forEach(img => {
     img.addEventListener('click', (a) => {
         let src = a.target.getAttribute('src')
         fondo.style.backgroundImage = `url(${src})`;
-        fondo.style.backgroundSize = 'cover'
-    })
+        fondo.style.backgroundSize = 'cover';
+    });
 });
 
 prev.addEventListener('click', () => {
     slider.scrollLeft -= 253;
-})
+});
 
 next.addEventListener('click', () => {
     slider.scrollLeft += 252;
-})
-
-const labelName = document.querySelector('.form__label-name');
-const formName = document.querySelector('.form__name');
-
-formName.addEventListener('click', (a) => {
-    console.log(a);
-})
-
-
-console.log(labelName);
-console.log(formName);
+});
