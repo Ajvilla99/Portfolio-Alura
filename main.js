dbb952008d8453706836b51b94e70ae989034aa2
Abrahan <ajvilla1999@gmail.com>
2023-07-13 19:20:41 -0500

    add interactividad menu mobile

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
-const slider = document.querySelector('.slider');
-const imagenes = document.querySelectorAll('.img_proyect');
-const fondo = document.getElementById('slider_bg');
+// const prev = document.querySelector('.prev');
+// const next = document.querySelector('.next');
+// const slider = document.querySelector('.slider');
+// const imagenes = document.querySelectorAll('.img_proyect');
+// const fondo = document.getElementById('slider_bg');
 
-imagenes.forEach(img => {
-    img.addEventListener('click', (a) => {
-        let src = a.target.getAttribute('src')
-        fondo.style.backgroundImage = `url(${src})`;
-        fondo.style.backgroundSize = 'cover';
-    });
-});
+// imagenes.forEach(img => {
+//     img.addEventListener('click', (a) => {
+//         let src = a.target.getAttribute('src')
+//         fondo.style.backgroundImage = `url(${src})`;
+//         fondo.style.backgroundSize = 'cover';
+//     });
+// });
 
-prev.addEventListener('click', () => {
-    slider.scrollLeft -= 253;
-});
+// prev.addEventListener('click', () => {
+//     slider.scrollLeft -= 253;
+// });
 
-next.addEventListener('click', () => {
-    slider.scrollLeft += 252;
-});
+// next.addEventListener('click', () => {
+//     slider.scrollLeft += 252;
+// });
+
+const menuMobile = document.querySelector('.menu-mobile');
+console.log(menuMobile.classList);
+menuMobile.addEventListener('click', () => {
+    menuMobile.classList.toggle('bx-x')
+})
